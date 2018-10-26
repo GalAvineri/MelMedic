2b6f0983e1a3f58ec50633bf74025b4c96cca72a
Gal Avineri <gal.avineri@gmail.com>
2018-10-26 16:11:55 +0300

    Added the ability to save the best model created during the training process

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from data.dataset import Dataset
+from auxilleries import IO
 
 from tensorflow.keras.applications.inception_v3 import InceptionV3
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
-from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping
+from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint
 from sklearn.metrics import classification_report
 from sklearn.utils import class_weight
 import numpy as np
@@ -11,7 +12,6 @@ import os
 from os.path import join
 batch_size = 32
 epochs = 100
-
 
 # # Count number of train and val samples
 # train_num_samples = sum([len(files) for r, d, files in os.walk(train_dir)])
@@ -39,11 +39,13 @@ test = Dataset(join(data_dir, 'test'))
 
 train_iter = train.dataset.shuffle(1000).batch(batch_size).repeat().prefetch(batch_size)
 val_iter = val.dataset.shuffle(1000).batch(batch_size).repeat().prefetch(batch_size)
-test_iter = test.dataset.batch(batch_size, drop_remainder=True).prefetch(batch_size)
+test_iter = test.dataset.batch(batch_size).prefetch(batch_size)
 
 # Training configurations
 class_weights = class_weight.compute_class_weight('balanced', np.unique(train.labels), train.labels)
-callbacks = [EarlyStopping(patience=10), ReduceLROnPlateau()]
+callbacks = [EarlyStopping(patience=10), ReduceLROnPlateau(),
+             ModelCheckpoint(join(os.pardir, 'Models', 'best_model.h5'), save_best_only=True)]
+IO.create_if_none(join(os.pardir, 'Models'))
 
 # Train the model
 model.fit(train_iter, validation_data=val_iter, epochs=epochs,
@@ -54,7 +56,5 @@ model.fit(train_iter, validation_data=val_iter, epochs=epochs,
 
 # Evaluate the model
 preds = model.predict(test_iter, steps=test.size // batch_size)
-test_labels = test.labels
-test_labesl = test_labels[:(test.size // batch_size) * batch_size] # truncate to the number of predictions made
 preds = np.argmax(preds, axis=1)
-print(classification_report(test_labels, preds))
+print(classification_report(test.labels[:len(preds)], preds))
