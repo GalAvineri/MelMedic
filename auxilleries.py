af9142d0fabd86b6c87a345a49dfb167b9765225
GalAvineri <pudinda@gmail.com>
2018-11-04 13:55:05 +0200

    Fixed bug: wasn't fully reproducible . added random.seed as well

--- auxilleries.py
+++ auxilleries.py
@@ -39,7 +39,9 @@ class MultiProcessing:
 class Reproduction:
     @staticmethod
     def make_run_reproducible():
-        from numpy.random import seed
-        from tensorflow import set_random_seed
+        from random import seed
         seed(1)
-        set_random_seed(2)
+        import numpy.random
+        numpy.random.seed(2)
+        from tensorflow import set_random_seed
+        set_random_seed(3)
